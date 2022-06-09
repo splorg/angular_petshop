3d5763e5949f870690ff53dfb49ba5e06a453323
gabriel.lima <gabriel.limabs02@gmail.com>
2022-06-09 20:28:46 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupPageComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    PetsPageComponent,
+    ProductsPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-page',
+  templateUrl: './products-page.component.html'
+})
+export class ProductsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
